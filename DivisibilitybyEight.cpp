bd4cf5cfaf276bc35d4630332832700ce62f104f
alberth7 <maikeru.bash@gmail.com>
2018-07-03 10:35:12 -0400

    fisrt exercices

---
+++ DivisibilitybyEight.cpp
@@ -0,0 +1,50 @@
+#include <bits/stdc++.h>
+	using namespace std;
+	 void mostrar(int v[],int n){
+	 	for (int i = 0; i < n; ++i)
+	 	{
+	 		cout<<(v[i]);
+	 	}
+	 	cout<<endl;
+	 }
+
+	 int sumar(int v[],int n){
+	 	int s=0;
+	 	for (int i = 0; i <n; ++i)
+	 	{
+	 		s=s+v[i];
+	 	}
+
+	 	return s;
+	 }
+	int main (){
+		int n,x,suma=0,suma2=0;
+		cin>>n;
+		int v[n];
+		for (int i = 0; i < n; ++i)
+		{
+			cin>>x;
+			v[i]=x;
+		}
+		//mostrar(v,n);
+		sort(v,v+n);
+		int stMenor=sumar(v,n);
+		int stMayor=0;
+		int cm=1;
+		
+		for (int i = n-1; i >= 0; i--)
+		{
+			stMayor=stMayor+v[i];
+			stMenor=stMenor-v[i];
+		
+			if(stMayor<=stMenor){
+				cm++;
+			}
+		}
+		cout<<(cm)<<endl;
+		
+
+	 return 0;
+	 
+	
+	}
